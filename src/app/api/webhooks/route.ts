b9e49ebef082e4236496d6c6d32261e96144917b
Tiago Fernandes <tiago.fernandes@timeprimo.com>
2024-06-17 15:55:08 -0300

    updata

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -64,7 +64,7 @@ export async function POST(req: Request) {
       })
 
       await resend.emails.send({
-        from: 'CaseCobra <hello@joshtriedcoding.com>',
+        from: 'Tiago Teste <tiagofer1998@gmail.com>',
         to: [event.data.object.customer_details.email],
         subject: 'Thanks for your order!',
         react: OrderReceivedEmail({
