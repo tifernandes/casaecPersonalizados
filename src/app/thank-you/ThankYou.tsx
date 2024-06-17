879af5325d92e1b84705a1b4cbdd95d8918913b8
Tiago Fernandes <tiago.fernandes@timeprimo.com>
2024-06-17 15:10:50 -0300

    updata

--- src/app/thank-you/ThankYou.tsx
+++ src/app/thank-you/ThankYou.tsx
@@ -23,20 +23,20 @@ const ThankYou = () => {
       <div className='w-full mt-24 flex justify-center'>
         <div className='flex flex-col items-center gap-2'>
           <Loader2 className='h-8 w-8 animate-spin text-zinc-500' />
-          <h3 className='font-semibold text-xl'>Loading your order...</h3>
-          <p>This won't take long.</p>
+          <h3 className='font-semibold text-xl'>Carregando sua compra...</h3>
+          <p>Nao ira demorar.</p>
         </div>
       </div>
     )
   }
 
-  if (data === false) {
+  if (data === null) {
     return (
       <div className='w-full mt-24 flex justify-center'>
         <div className='flex flex-col items-center gap-2'>
           <Loader2 className='h-8 w-8 animate-spin text-zinc-500' />
-          <h3 className='font-semibold text-xl'>Verifying your payment...</h3>
-          <p>This might take a moment.</p>
+          <h3 className='font-semibold text-xl'>Verificando seu pagamento...</h3>
+          <p>Nao ira demorar.</p>
         </div>
       </div>
     )
