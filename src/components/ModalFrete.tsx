c1de010d9ea42844ffb4bc1cfd19176957e61108
Tiago Fernandes <tiago.fernandes@timeprimo.com>
2024-07-29 00:00:35 -0300

    updata

--- src/components/ModalFrete.tsx
+++ src/components/ModalFrete.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from '@/components/ui/dialog'
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -23,7 +24,6 @@ import {
 
 import { useEffect, useState, useCallback } from 'react'
 import { debounce } from 'lodash';
-import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import { Button } from '@/components/ui/button'
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
@@ -123,7 +123,7 @@ export default function ModalFrete({
         });
     
         const data = await res.json();
-        setLogistica(data.result.ShippingSevicesArray);
+        setLogistica(data.result.ShippingSevicesArray.filter((i: logistica) => i.ServiceCode != "04227"));
         console.log(data)
     }
 
@@ -183,17 +183,17 @@ export default function ModalFrete({
     })
 
   return (
-    <Dialog onOpenChange={setIsFrenetModal} open={isFrenetModal}>
-    <DialogContent className='lg:max-w-screen-lg overflow-y-scroll max-h-screen z-[9999999]'>
-      <DialogHeader>
-        <DialogTitle className='text-3xl text-center font-bold tracking-tight text-gray-900'>
+    <Sheet onOpenChange={setIsFrenetModal} open={isFrenetModal}>
+    <SheetContent className='overflow-y-scroll max-h-screen sm:max-w-[650px] w-[400px] sm:w-[650px] z-[9999999]'>
+      <SheetHeader>
+        <SheetTitle className='text-3xl text-center font-bold tracking-tight text-gray-900'>
           Entrega
-        </DialogTitle>
-        <DialogDescription className='text-base text-center py-2'>
+        </SheetTitle>
+        <SheetDescription className='text-base text-center py-2'>
           <span className='font-medium text-zinc-900'>
             Preencha as informaçoes de entrega
           </span>
-        </DialogDescription>
+        </SheetDescription>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
           <div className="grid w-full items-center gap-1.5">
@@ -201,7 +201,7 @@ export default function ModalFrete({
               control={form.control}
               name="cep"
               render={({ field }) => (
-                <FormItem>
+                <FormItem className="flex items-start flex-col">
                   <FormLabel>CEP</FormLabel>
                   <FormControl>
                     <Input placeholder="digite seu CEP" onChangeCapture={handleCEPCalc} {...field} />
@@ -221,7 +221,7 @@ export default function ModalFrete({
                       control={form.control}
                       name="cidade"
                       render={({ field }) => (
-                        <FormItem>
+                        <FormItem className="flex items-start flex-col">
                           <FormLabel>Cidade</FormLabel>
                           <FormControl>
                             <Input placeholder="Cidade" defaultValue={field.value} {...field} />
@@ -236,7 +236,7 @@ export default function ModalFrete({
                       control={form.control}
                       name="Bairro"
                       render={({ field }) => (
-                        <FormItem>
+                        <FormItem className="flex items-start flex-col">
                           <FormLabel>Bairro</FormLabel>
                           <FormControl>
                             <Input placeholder="Bairro" defaultValue={field.value} {...field} />
@@ -251,7 +251,7 @@ export default function ModalFrete({
                       control={form.control}
                       name="Rua"
                       render={({ field }) => (
-                        <FormItem>
+                        <FormItem className="flex items-start flex-col">
                           <FormLabel>Rua</FormLabel>
                           <FormControl>
                             <Input placeholder="Rua" defaultValue={field.value} {...field} />
@@ -266,7 +266,7 @@ export default function ModalFrete({
                       control={form.control}
                       name="Estado"
                       render={({ field }) => (
-                        <FormItem>
+                        <FormItem className="flex items-start flex-col">
                           <FormLabel>Estado</FormLabel>
                           <FormControl>
                             <Input placeholder="Estado" defaultValue={field.value} {...field} />
@@ -283,7 +283,7 @@ export default function ModalFrete({
                     control={form.control}
                     name="Complemento"
                     render={({ field }) => (
-                      <FormItem>
+                      <FormItem className="flex items-start flex-col">
                         <FormLabel>Complemento (opcional)</FormLabel>
                         <FormControl>
                           <Input placeholder="Complemento" {...field} />
@@ -301,7 +301,7 @@ export default function ModalFrete({
                   render={({ field }) => (
                     <FormItem className="space-y-3">
                       <FormControl>
-                      <RadioGroup onValueChange={field.onChange}>
+                      <RadioGroup onValueChange={field.onChange} className="gap-5">
                         {logistica.map((logi) => {
                           return(
                             <div key={logi.ServiceCode} className="flex items-center space-x-2">
@@ -310,10 +310,12 @@ export default function ModalFrete({
                                   <RadioGroupItem value={logi.ServiceDescription + - + logi.ShippingPrice} />
                                 </FormControl>
                                 <FormLabel className="font-normal">
-                                  <div className="grid md:grid-cols-3 grid-cols-2 gap-4">
-                                    <p>{logi.ServiceDescription} </p>
-                                    <p>R${logi.ShippingPrice} </p>
-                                    <p className='md:block none'>Entrega em {logi.DeliveryTime} dias uteis</p>
+                                  <div className="grid md:grid-cols-2 grid-cols-2 gap-10">
+                                    <p className="flex gap-1.5 flex-col w-[140px]">
+                                      {logi.ServiceDescription}
+                                      <p className='text-green-600'>{logi.DeliveryTime} dias uteis</p>
+                                    </p>
+                                    <p className="flex items-center justify-center">R${logi.ShippingPrice} </p>
                                   </div>
                                 </FormLabel>
                               </FormItem>
@@ -327,23 +329,25 @@ export default function ModalFrete({
                   )}
                 />
               </div>
-              {!loadingSeguirPag ? (
-              <Button
-              className='px-4 sm:px-6 lg:px-8'>
-                Seguir ao pagamento <ArrowRight className='h-4 w-4 ml-1.5 inline' />
-              </Button>
-              ) : (
-                <Button disabled className='px-4 sm:px-6 lg:px-8'>
-                  <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-                  Redirecionando
+              <div className="w-full flex items-end justify-end">
+                {!loadingSeguirPag ? (
+                <Button
+                className='px-4 w-full sm:px-6 lg:px-8'>
+                  Seguir ao pagamento <ArrowRight className='h-4 w-4 ml-1.5 inline' />
                 </Button>
-              )}
+                ) : (
+                  <Button disabled className='px-4 w-full sm:px-6 lg:px-8'>
+                    <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                    Redirecionando
+                  </Button>
+                )}
+              </div>
             </div>
           )}
           </form>
         </Form>
-      </DialogHeader>
-    </DialogContent>
-  </Dialog>
+      </SheetHeader>
+    </SheetContent>
+  </Sheet>
   )
 }
